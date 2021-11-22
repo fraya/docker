8611e56a3a80b5e4be5b039cf536799d55ea027b
Ingo Albrecht <commit@promovicz.org>
2021-11-22 21:58:48 +0100

    Make the images share a name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -f Dockerfile.opendylan-release --network host -t opendylan-release:latest .
-docker build -f Dockerfile.opendylan-current --network host -t opendylan-current:latest .
+docker build -f Dockerfile.opendylan-release --network host -t opendylan:release .
+docker build -f Dockerfile.opendylan-current --network host -t opendylan:current -t opendylan:latest .
 
