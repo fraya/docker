24089e04030e521c3003bfbb5e691fe52ff65993
Ingo Albrecht <commit@promovicz.org>
2021-11-29 18:17:37 +0100

    Build some images containing dylan-tool

--- build.sh
+++ build.sh
@@ -3,15 +3,30 @@
 docker build --network host \
        -f Dockerfile.opendylan-release \
        --build-arg "OPENDYLAN_BASE=debian:bullseye" \
-       -t opendylan:release \
-       -t opendylan:release-bullseye \
+       -t opendylan:release-base \
+       -t opendylan:release-base-bullseye \
        .
 
 docker build --network host \
        -f Dockerfile.opendylan-current \
        --build-arg "OPENDYLAN_BASE=debian:bullseye" \
+       -t opendylan:current-base \
+       -t opendylan:current-base-bullseye \
+	   .
+
+docker build --network host \
+       -f Dockerfile.opendylan-devel \
+       --build-arg "OPENDYLAN_BASE=opendylan:release-base-bullseye" \
+       -t opendylan:release \
+       -t opendylan:release-bullseye \
+       empty
+
+docker build --network host \
+       -f Dockerfile.opendylan-devel \
+       --build-arg "OPENDYLAN_BASE=opendylan:current-base-bullseye" \
        -t opendylan:current \
        -t opendylan:current-bullseye \
        -t opendylan:latest \
        -t opendylan:latest-bullseye \
-       .
+       empty
+
